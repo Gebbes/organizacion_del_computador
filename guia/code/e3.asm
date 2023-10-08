65d0c32de6db67b42fbf7ddaf7f85ed4073ea948
Gebbes <gonzaloebbes@gmail.com>
2023-10-08 18:46:17 -0300

    codigo inestable, error de seg fault/ error scanf

--- guia/code/e3.asm
+++ guia/code/e3.asm
@@ -1,31 +1,68 @@
 global main
-global puts
+extern puts, gets, sscanf, printf
 
 ; X elevado a Y, considerando todos los casos, 1 ++, 2 +-, 3 -+, 4 --
 
-section .bss
-    X db 50
-    Y db 50
-
 section .data
-    ingTexto "Ingrese dos numeros para hacer X elevado a Y, separados por un espacio: ",0
+    ingTexto dq "Ingrese dos numeros para hacer X elevado a Y, separados por un espacio: ",0
+    formatoIngresado dq "%d %d", 0
+    msjError dq "Hubo un error en el programa", 0
+    intFormat dq "%d",0
+    debug dq "hasta aca llegue", 0
+    debugA dq "hasta aca llegueA", 0
+    debugB dq "hasta aca llegueB", 0
+    debugC dq "hasta aca llegueC", 0
 
+section .bss
+    X resq 1
+    Y resq 1
+    valoresIngresados resb 50
+
+
+section .text
 main:
 
     mov rdi, ingTexto
+    sub rsp, 8
     call puts
+    add rsp, 8
 
-ret
+    mov rdi,  valoresIngresados
+    sub rsp, 8
+    call gets
+    add rsp, 8
+
+    sub rsp, 8
+    mov rdi, formatoIngresado
+    mov rsi, X
+    mov rdx, Y
+    call sscanf
+    
+    cmp rax, 2
+    jne error1
+
+    add rsp, 8
+
+    mov rdi, debugB
+    call puts
+    mov rdi, intFormat
+    mov rsi, X
+    sub rsp, 8
+    call printf
+    add rsp, 8
+    mov rdi, debugA
 
 
-applyA:
-ret
+    ret
 
-ApplyB:
-ret
+error1:    
+    mov rdi, debug
+    call puts
+    mov rdi, intFormat
+    call printf
+    mov rdi, debug
+    mov rdi, msjError
+    call puts
+    ret
 
-ApplyC:
-ret
 
-ApplyD:
-ret
