08b0c3a318ff1cc46b5096ef8b348c62b12fd858
Gebbes <gonzaloebbes@gmail.com>
2023-10-08 22:38:09 -0300

    añadidos casos bordes de x=0 e y=0

--- guia/code/e3.asm
+++ guia/code/e3.asm
@@ -4,14 +4,23 @@ extern puts, gets, sscanf, printf
 ; X elevado a Y, considerando todos los casos, 1 ++, 2 +-, 3 -+, 4 --
 
 section .data
-    ingTexto dq "Ingrese dos numeros para hacer X elevado a Y, separados por un espacio: ",0
-    formatoIngresado dq "%d %d", 0
-    msjError dq "Hubo un error en el programa", 0
-    intFormat dq "%d",0
-    debug dq "hasta aca llegue", 0
-    debugA dq "hasta aca llegueA", 0
-    debugB dq "hasta aca llegueB", 0
-    debugC dq "hasta aca llegueC", 0
+    ingTexto db "Ingrese dos numeros para hacer X elevado a Y, separados por un espacio: ",0
+    formatoIngresado db "%d %d", 0
+
+    ; Errores:
+    msjError db "Hubo un error en el programa", 0
+    msjError00 db "Error: 0 elevado a la 0 no es una operacion permitida",0
+
+    ; Debbugin
+    debugA db "ACA A", 0
+    debugB db "ACA B", 0
+    debugC db "ACA C", 0
+    debugD db "ACA D", 0
+
+    ; Resultado
+    msjFinal db "El resultado es: %d", 0
+
+
 
 section .bss
     X resq 1
@@ -21,48 +30,71 @@ section .bss
 
 section .text
 main:
-
-    mov rdi, ingTexto
+    mov rdi, ingTexto ;Ingreso de los numeros
     sub rsp, 8
     call puts
     add rsp, 8
 
-    mov rdi,  valoresIngresados
+    mov rdi, valoresIngresados
     sub rsp, 8
     call gets
     add rsp, 8
 
-    sub rsp, 8
-    mov rdi, formatoIngresado
-    mov rsi, X
-    mov rdx, Y
+    sub rsp, 8 ;Formateo de los numeros
+    mov rdi, valoresIngresados
+    mov rsi, formatoIngresado
+    mov rdx, X
+    mov rcx, Y
     call sscanf
+    add rsp, 8
     
     cmp rax, 2
     jne error1
 
-    add rsp, 8
+    mov rax, [Y] ;Analisis del input
+    cmp rax, 0
+    je casoY0
 
-    mov rdi, debugB
-    call puts
-    mov rdi, intFormat
-    mov rsi, X
-    sub rsp, 8
-    call printf
-    add rsp, 8
-    mov rdi, debugA
-
+    mov rax, [X]
+    cmp rax, 0
+    je casoX0
 
     ret
 
-error1:    
-    mov rdi, debug
-    call puts
-    mov rdi, intFormat
-    call printf
-    mov rdi, debug
+error1:
     mov rdi, msjError
     call puts
     ret
+
+casoY0:
+    mov rax, [X]
+    cmp rax, 0
+    je error00
+    mov rdi, msjFinal
+    mov rsi, 1
+    sub rsp, 8
+    call printf
+    add rsp, 8
+    call puts
+    ret
+
+casoX0:
+    mov rdi, msjFinal
+    mov rsi, 0
+    sub rsp, 8
+    call printf
+    add rsp, 8
+    call puts
+    ret
+
+error00:
+    mov rdi, msjError00
+    sub rsp, 8
+    call puts
+    add rsp, 8
+    ret
+
+
+
 
 
