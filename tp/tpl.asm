75a2d535af898ef7f8cca002bb3da81aceaa52d2
Gebbes <gonzaloebbes@gmail.com>
2023-10-30 23:28:16 -0300

    escribiendo en linux

--- tp/tpl.asm
+++ tp/tpl.asm
@@ -11,56 +11,55 @@ section .data
     
 section .bss
     input resb 50
-
-    gDia resq 1
-    gMes resq 1
-    gAño resq 1
+    
+    gDia resq 0
+    gMes resq 0
+    gAño resq 0
 
 section .text
-
 main:
-    mov rcx, ingFecha
-    sub rsp, 32
+    mov rdi, ingFecha
+    sub rsp, 8
     call printf
-    add rsp, 32
+    add rsp, 8
 
-    sub rsp, 32
-    mov rcx, input
+    sub rsp, 8
+    mov rdi, input
     call gets
-    add rsp, 32
+    add rsp, 8
 
-    sub rsp, 32
-    mov rcx, input
-    mov rdx, formatoGreg
-    mov r8, gDia
-    mov r9, gMes
-    mov r10, gAño
+    sub rsp, 8
+    mov rdi, input
+    mov rsi, formatoGreg
+    mov rdx, gDia
+    mov rcx, gMes
+    mov r8, gAño
     call sscanf
-    add rsp, 32
+    add rsp, 8
 
     cmp rax, 3
     je printFecha
     
-    mov rcx, err
-    sub rsp, 32
+    mov rdi, err
+    sub rsp, 8
     call printf
-    add rsp, 32
+    add rsp, 8
 
     ret
 
 printFecha:
 
-    mov rcx, ingFecha
-    sub rsp, 32
+    mov rdi, ingFecha
+    sub rsp, 8
     call printf
-    add rsp, 32
+    add rsp, 8
 
     
-    mov rcx, formatoGreg
-    mov rdx, gDia
-    mov r8, gMes
-    mov r9, gAño
-    sub rsp, 32
+    mov rdi, formatoGreg
+    mov rsi, gDia
+    mov rdx, gMes
+    mov rcx, gAño
+    sub rsp, 8
     call printf
-    add rsp, 32
+    add rsp, 8
     ret
