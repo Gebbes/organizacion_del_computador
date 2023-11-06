74dfe094e770ab4498202c466fa3f7f19fa22c7b
Gebbes <gonzaloebbes@gmail.com>
2023-11-06 18:29:38 -0300

    avances del tp

--- tp/tpl.asm
+++ tp/tpl.asm
@@ -1,65 +1,124 @@
 global main
-extern puts, gets, sscanf, printf
+extern puts, gets, sscanf, printf, checkAlign
 
 section .data
-    ingFecha dq "Ingrese una fecha: ", 0
-    formatoGreg dq "%d/%d/%d", 0
-    formatoRom dq "%s/%s", 0
-    formatoJul dq "%d/%d", 0
+    ingFecha dq "Ingrese una fecha ( N para detener el programa ) : ", 0
+    cierre dq "X",0
+    formatoGreg dq "%d/%d/%d",10, 0
+    formatoJul dq "%d/%d",10, 0
+    formatoRom dq "%s/%s/%s",10, 0
 
-    err dq "Hubo un error", 13, 0
+    msjG db "El formato es Gregoriano!",0
+    msjJ db "El formato es Juliano!",0
+    msjR db "El formato es Romano!",0
+
+
+    err dq "Hubo un error", 0
     
 section .bss
     input resb 50
     
-    gDia resq 0
-    gMes resq 0
-    gAño resq 0
+    gDia resq 1
+    gMes resq 1
+    gAnio resq 1
+
+    jDia resq 1
+    jAnio resq 1
+
+    rDia resb 20
+    rMes resb 20
+    rAnio resb 20
 
 section .text
 main:
     mov rdi, ingFecha
-    sub rsp, 8
+    sub rax, rax
     call printf
-    add rsp, 8
 
-    sub rsp, 8
     mov rdi, input
     call gets
-    add rsp, 8
 
-    sub rsp, 8
+    cmp byte[input], 'N'
+    jne scanInput
+    ret
+
+scanInput:
     mov rdi, input
     mov rsi, formatoGreg
     mov rdx, gDia
     mov rcx, gMes
-    mov r8, gAño
+    mov r8, gAnio
     call sscanf
-    add rsp, 8
 
     cmp rax, 3
-    je printFecha
+    je isGregorian
+
+    mov rdi, input
+    mov rsi, formatoJul
+    mov rdx, jDia
+    mov rcx, jAnio
+    call sscanf
     
+    cmp rax, 2
+    je isJulian
+
+    ;sub rsp, 8
+    ;mov rdi, input
+    ;mov rsi, formatoRom
+    ;mov rdx, rDia
+    ;mov rcx, rMes
+    ;mov r8, rAnio
+    ;call sscanf
+    ;add rsp, 8
+
+    ;cmp rax, 3
+    ;je isRoman
+
+    ; manual scan of roman case
+
+
     mov rdi, err
-    sub rsp, 8
+    sub rax, rax
     call printf
-    add rsp, 8
 
     ret
 
-printFecha:
+isGregorian:
+    mov rdi, msjG
+    call puts
 
-    mov rdi, ingFecha
-    sub rsp, 8
-    call printf
-    add rsp, 8
-
-    
     mov rdi, formatoGreg
-    mov rsi, gDia
-    mov rdx, gMes
-    mov rcx, gAño
-    sub rsp, 8
+    mov rsi, [gDia] 
+    mov rdx, [gMes]
+    mov rcx, [gAnio]
+    sub rax, rax
     call printf
-    add rsp, 8
-    ret
+
+    jmp clean
+
+isJulian:
+    mov rdi, msjJ
+    call puts
+
+    mov rdi, formatoJul
+    mov rsi, [jDia] 
+    mov rcx, [jAnio]
+    sub rax, rax
+    call printf
+    jmp clean
+
+isRoman:
+    mov rdi, msjR
+    call puts
+
+    mov rdi, formatoRom
+    mov rsi, rDia
+    mov rdx, rMes
+    mov rcx, rAnio
+    sub rax, rax
+    call printf
+    jmp clean
+
+clean:
+    ;restart any needed memory
+    jmp main
